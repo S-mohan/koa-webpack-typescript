e08b830866eafc7971ec7513d279ca86ba5c1ead
smohan <smohan@163.com>
2019-06-25 14:33:42 +0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const plugins = [
+  require('autoprefixer')({
+    
+  }),
+  require('cssnano')({
+    preset: [
+      'default', {
+        discardComments: {
+          removeAll: true
+        }
+      }
+    ]
+  })
+]
+
+module.exports = {
+  plugins
+}
